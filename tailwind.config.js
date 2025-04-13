502cc59cd40c043a54c21142f448ea32562e8e80
Asiful Haque <73637081+Asiful-Haque@users.noreply.github.com>
2025-04-13 10:05:42 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',       //////////Now use app router
+      './components/**/*.{js,ts,jsx,tsx}',  //////////Now use app router
     ],
     theme: {
       extend: {},
     },
     plugins: [],
   };
+
+// module.exports = {
+//   content: [
+//     './src/app/**/*.{js,ts,jsx,tsx,mdx}',  // Only scan App Router content
+//     './src/components/**/*.{js,ts,jsx,tsx}',  // Components folder
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
   
